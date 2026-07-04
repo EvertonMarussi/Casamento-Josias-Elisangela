d4049f2e653fb7149651e2ab224eae42e50a19d2
EvertonMarussi <evertonmarussi@gmail.com>
2026-07-04 10:13:14 -0300

    feat: corrigindo rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import Home from '../views/LandingHome.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'LandingHome',
-    component: Home
-  },
-]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes: [
+    {
+      path: '/site',
+      name: 'LandingHome',
+      component: Home
+    }
+  ]
 })
 
 export default router
